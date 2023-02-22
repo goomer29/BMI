1f4f2e054f6ad930cb8710179c5dbbd0463fc0e2
omergo hi <omergolan92@gmail.com>
2023-02-23 01:44:31 +0200

    Add project files.

---
+++ BMI/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace BMI;
+
+public partial class MainPage : ContentPage
+{
+	
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+	public void ButtonClicked(object sender, EventArgs e)
+	{
+		double result = whightStepper.Value / Math.Pow(hightSlider.Value / 100, 2);
+        bmi.Text = "BMI result: "+(result).ToString("F2");
+		if (result < 18.5)
+		{
+			bmi.TextColor = Color.FromArgb("#0000FF");
+
+        }
+		else if (result >= 18.5 && result < 25)
+		{
+            bmi.TextColor = Color.FromArgb("#008000");
+        }
+        else if (result >= 25 && result <= 29.9)
+        {
+            bmi.TextColor = Color.FromArgb("#FF0000");
+        }
+        else if (result >= 30 && result <= 39.9)
+        {
+            bmi.TextColor = Color.FromArgb("#800000");
+        }
+        else
+        {
+            bmi.TextColor = Color.FromArgb("#000000");
+        }
+    }
+}
+
